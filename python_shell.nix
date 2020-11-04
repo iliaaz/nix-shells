1bffd54112ef379bf89d0a712a72cfa277685904
Ilia Auerbach-Ziogas <ilia.auerbach.ziogas@gmail.com>
2020-11-03 20:40:04 -0500

    playing2

--- python_shell.nix
+++ python_shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import (fetchTarball https://github.com/nixos/nixpkgs/archive/nixpkgs-unstable.tar.gz) {} }:
 
 pkgs.mkShell {
-  buildInputs = [ pkgs.python38 pkgs.jq pkgs.poetry ];
+  buildInputs = [ pkgs.python38 pkgs.jq pkgs.git pkgs.poetry ];
 }
